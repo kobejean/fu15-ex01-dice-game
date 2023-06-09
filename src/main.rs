0378d7b36c89961297bc0a7dba097e9e1e3c3e7d
Jean Flaherty <kobejean@me.com>
2023-06-09 12:46:14 +0900

    Dice game: initial revision

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use rand::{Rng, rngs::ThreadRng};
+
+fn roll(rng: &mut ThreadRng, die: u32) -> u32 {
+    let num = rng.gen_range(1..=6);
+    println!("Die {}: {}", die, num);
+    return num;
+}
+
+fn main() {
+    let num_rolls: u32 = 2;
+    let mut rng = rand::thread_rng();
+
+    println!("Rolling dice...");
+    let mut sum = 0;
+    for n in 1..=num_rolls {
+        sum += roll(&mut rng, n);
+    }
+    println!("Total value: {}", sum);
+}
